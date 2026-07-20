26225e8dfe1bec7ac8b2affa6a1dbe890f796a32
danarseptiyanto <septiyandanar@gmail.com>
2026-07-20 08:45:25 +0700

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<p className="text-red-500 font-bold">hehe</p>
+		</div>
+	);
+}
+
+export default App;
