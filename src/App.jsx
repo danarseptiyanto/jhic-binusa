1bc870836dcaedc8d31b8a1e36cb9fb9e885e373
danarseptiyanto <septiyandanar@gmail.com>
2026-07-21 08:25:45 +0700

    Add Google Fonts and ProfilSekolah component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import React from "react";
 import "./App.css";
+import ProfilSekolah from "./components/ProfilSekolah";
 
 function App() {
 	return (
 		<div>
 			<p className="text-red-500 font-bold">hehe</p>
+			<ProfilSekolah />
 		</div>
 	);
 }
